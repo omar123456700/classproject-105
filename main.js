e24c578008a21bdb9ccbd286091ca474a8c5f128
omar123456700 <73769715+omar123456700@users.noreply.github.com>
2021-06-12 10:34:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function capture()
          });
 }
 console.log('ml5 version:',ml5.version);
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/.json',modelLoaded);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/LRc4mvv-R/.json',modelLoaded);
 function modelLoaded(){
     console.log('Model Loaded!');
 }
